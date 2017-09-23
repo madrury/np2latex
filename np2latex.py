45a31d7e5c4b92ac27f928a5330ef026f56c4543
madrury <matthew.drury.83@gmail.com>
2017-09-23 16:49:33 -0700

    Clean up row ending string for row vectors.

--- np2latex.py
+++ np2latex.py
@@ -5,17 +5,17 @@ def np2latex(arr, column=True):
         if column:
             return _make_matrix(arr.reshape(-1, 1))
         else:
-            return _make_matrix(arr.reshape(1, -1))
+            return _make_matrix(arr.reshape(1, -1), end_str="")
     else:
         raise ValueError("Array must be 1 or 2 dimensional.")
     
-def _make_matrix(arr):
+def _make_matrix(arr, **kwargs):
     n_cols = arr.shape[1]
     left = "\\left( \\begin{{array}}{{{}}} ".format('c'*arr.shape[1])
     right = " \\end{array} \\right)"
-    rows = [_make_row_format_string(n_cols).format(*row)
+    rows = [_make_row_format_string(n_cols, **kwargs).format(*row)
             for row in arr]
     return left + ' '.join(rows) + right
 
-def _make_row_format_string(n_columns, format_str=":2.2f", join_str=" & "):
-    return join_str.join(["{" + format_str + "}"]*n_columns) + r" \\"
+def _make_row_format_string(n_columns, format_str=":2.2f", end_str=r" \\"):
+    return " & ".join(["{" + format_str + "}"]*n_columns) + end_str
